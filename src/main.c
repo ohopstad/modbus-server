c53222a3deeb0050affb2d10c366d66ca5a7e68e
Olav Opstad <22353118+ohopstad@users.noreply.github.com>
2023-05-06 14:01:33 +0200

    Added client name-finding function. Still segfault

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 #include<stdlib.h>
 #include<pthread.h>
 #include<errno.h>
+#include<netdb.h>
 #ifdef _WIN32
     #include<winsock.h>
 #else
@@ -47,24 +48,35 @@ struct options ReadFlags(int argc, char ** argv)
     return ret;
 };
 
+int FindClientName(int socket, char* name, int name_len)
+{
+    struct sockaddr_storage addr;
+#ifdef _WIN32
+    int addr_len = 0;
+#else 
+    socklen_t addr_len = 0;
+#endif
+
+    if (getpeername(socket, (struct sockaddr*)&addr, &addr_len) != 0)
+    {
+        // TODO: some errorhandling
+    }
+    if (getnameinfo((struct sockaddr*)&addr, addr_len, name, name_len, NULL, 0, NI_NUMERICHOST) != 0)
+    {
+        // TODO: more error handling
+    }
+    return 0;
+}
+
+
 // should handle the connection of one client in its own thread
 void* HandleConnection(void* arg)
 {
     modbus_t* client = (modbus_t*) arg;
-    struct sockaddr client_addr; 
 
-    #ifdef _WIN32
-        int name_len;
-    #else 
-        socklen_t name_len;
-    #endif
-
-    if (getpeername(modbus_get_socket(client), &client_addr, &name_len) != 0)
-    { // error getting name
-        fprintf(stderr, "could not fetch name of socket %d \n", modbus_get_socket(client));
-        return NULL;
-    }
-    printf("new connection from: %s\n", client_addr.sa_data);
+    char clientName[NI_MAXHOST];
+    FindClientName(modbus_get_socket(client), clientName, NI_MAXHOST);
+    printf("new connection from: %s\n", clientName);
 
     while (1)
     {
@@ -75,15 +87,23 @@ void* HandleConnection(void* arg)
         rx_count = modbus_receive(client, query);
         if (rx_count > 0)
         {
+            size_t queryString_len = (MODBUS_TCP_MAX_ADU_LENGTH *3);
+            char queryString[queryString_len];
+
+            for (int c = 0; c < rx_count ; c++)
+            {
+                sprintf(queryString, "%x ", query[c]);
+            }
+            printf("Query from %s:\n\t[%s]\n", clientName, queryString);
             pthread_mutex_lock(&db_access);
             status = modbus_reply(client, query, rx_count, db);
             pthread_mutex_unlock(&db_access);
             if (status != 0){
-                fprintf(stderr, "error %d replying to query from %s: %s\n", errno, client_addr.sa_data, modbus_strerror(errno));
+                fprintf(stderr, "error %d replying to query from %s: %s\n", errno, clientName, modbus_strerror(errno));
             }
         } else if (rx_count < 0){
             // error or closed
-            printf("%s disconnected\n", client_addr.sa_data);
+            printf("%s disconnected\n", clientName);
             return NULL; 
         } else {
             // empty query
